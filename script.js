ca5d80257938cf5c1ad24cced72082a256862325
Azaharul-islam615 <mdazaharul725@gmail.com>
2025-08-28 14:05:34 +0600

    add calling functionality

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ function countHeart(count){
     const heart=document.getElementById("love")
     heart.innerText=count
 }
+
+// call button part
+
+ const callBtns=document.querySelectorAll(".call-btn")
+ for(let callBtn of callBtns ){
+    callBtn.addEventListener("click",function(){
+        const numberOfCoin=document.getElementById("coin")
+        
+        let convertCoin=Number(numberOfCoin.innerText)
+        console.log(convertCoin)
+        const title=callBtn.parentNode.parentNode.children[1].innerText
+        const hotLine=callBtn.parentNode.parentNode.children[3].innerText
+        if(convertCoin>0){
+             alert(`Calling ${title} ${hotLine}....`)
+             convertCoin=convertCoin-20
+             numberOfCoin.innerText=convertCoin
+
+        }
+        else{
+            alert("insufficient coin")
+        }
+        addElement(title,hotLine)
+            
+ }) }
+
