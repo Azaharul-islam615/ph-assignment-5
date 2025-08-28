a40229df727fe201511a3b111aa3dbcdbaee6598
Azaharul-islam615 <mdazaharul725@gmail.com>
2025-08-28 16:20:34 +0600

    add clearing all call history

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function countHeart(count){
         const numberOfCoin=document.getElementById("coin")
         
         let convertCoin=Number(numberOfCoin.innerText)
-        console.log(convertCoin)
+        
         const title=callBtn.parentNode.parentNode.children[1].innerText
         const hotLine=callBtn.parentNode.parentNode.children[3].innerText
         if(convertCoin>0){
@@ -40,4 +40,34 @@ function countHeart(count){
         addElement(title,hotLine)
             
  }) }
+//  Add item in the call history 
+function addElement(title,hotLine){
+    const aside=document.getElementById("history")
 
+     const divParent=document.createElement("div")
+     divParent.innerHTML=` 
+    <div class="flex justify-between items-center pt-4 pb-4 pl-4 pr-4 bg-[#FAFAFA] rounded-lg">
+                <div>
+                    <h1 class="font-bold">${title}</h1>
+                    <p class="text-[#5C5C5C]">${hotLine}</p>
+                </div>
+                <p>11:36:58 AM</p>
+            </div>`
+            aside.appendChild(divParent)
+            divParent.classList.add("asideproperty")
+}
+
+// Celear button
+document.getElementById("clearAll").addEventListener("click",function(){
+  const asides=document.querySelectorAll(".asideproperty")
+   for(const aside of asides){
+    aside.remove()
+   }
+})
+
+// copy button
+
+function countNumberOfcopies(count){
+     const copyCounter=document.getElementById("count-copy").innerText
+     copyCounter=count
+}
