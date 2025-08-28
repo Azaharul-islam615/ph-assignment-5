200631584923abaa94058f636083f698d39a84b0
Azaharul-islam615 <mdazaharul725@gmail.com>
2025-08-28 11:49:16 +0600

    add responssive design and heard option

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 
+// heart part
 
+let count=0
+ const hearts =document.querySelectorAll(".heard")
+ for( const heartItem of hearts){
+    heartItem.addEventListener("click",function(){
+        count++
+        countHeart(count)
+    })
+    
+ }
+  
 
-
-
-
-
-
+function countHeart(count){
+    const heart=document.getElementById("love")
+    heart.innerText=count
+}
